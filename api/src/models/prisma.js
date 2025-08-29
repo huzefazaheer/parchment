0fd4113409b471fb89e9c18d61a513361a8b0e3e
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-08-30 01:39:03 +0500

    Deployment update

--- api/src/models/prisma.js
+++ api/src/models/prisma.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const { PrismaClient } = require('../../generated/prisma/client')
 
 const prisma = new PrismaClient({ log: ['warn', 'error'] })
+console.log('DATABASE', process.env.DATABASE_URL)
 
 module.exports = prisma
